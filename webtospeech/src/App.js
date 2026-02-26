082a1f1dece4a58218c99ee2e0aefe004b3b979c
chumbowum <chandlerboone2@icloud.com>
2026-02-26 11:18:20 -0600

    ReactRouter Setup
    Set up App.js to use index.js and App.js to use BrowserRouter, Routes, and Route. Page defaults to Login page. ReactTemplate accesible using "/reactTemplate" route.

--- webtospeech/src/App.js
+++ webtospeech/src/App.js
@@ -1,28 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import ReactTemplate from './pages/ReactTemplate';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {/*<p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>*/}
-          <p>Hello World! From CSCI 4805</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/reactTemplate" element={<ReactTemplate />} />
+    </Routes>
   );
 }
 
---
+++ webtospeech/src/pages/Login.js
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+      <div className="Login">
+       <h1>This will be the Login page</h1>
+      </div>
+  );
+}
+
+export default Login;
