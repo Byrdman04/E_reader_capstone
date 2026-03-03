e37c2345897ac45c84a36f9c1a3b6407c42149dd
chumbowum <chandlerboone2@icloud.com>
2026-03-03 12:27:58 -0600

    Attempted initial page
    Added basic HTML and CSS, created some folders, added Bootstrap. Lots of work to be done still, going to review Bootstrap and utilize React components to make this better.

--- webtospeech/src/pages/Profile.js
+++ webtospeech/src/pages/Profile.js
@@ -1,7 +1,24 @@
+import '../styles/profile.css';
+import pfp from '../resources/user-profile-icon.png';
+
 function Profile() {
     return (
         <div className="Profile">
-            <h1>This will be the Profile page</h1>
+            <div id="profile-banner">
+                <div className="column">
+                    {/*<h1>PFP Goes Here</h1>*/}
+                    <img id="profile-picture" src={pfp} alt="User's profile icon"></img>
+                    <p className="banner-text">User's Profile</p>
+                </div>
+
+                <div className="column">
+                    <h1>Documents Uploaded</h1>
+                    <p className="header-styling">42</p>
+                </div>
+            </div>
+            <div id="profile-collections-table">
+
+            </div>
         </div>
     );
 }
