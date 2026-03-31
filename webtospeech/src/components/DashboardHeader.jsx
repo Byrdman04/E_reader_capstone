1308a38f46dfb4f87774bd805e70eb6fd4f91184
TypicalJon <jhurley5@my.apsu.edu>
2026-03-31 13:58:40 -0500

    Functional Search bar

--- webtospeech/src/components/DashboardHeader.jsx
+++ webtospeech/src/components/DashboardHeader.jsx
@@ -1,7 +1,32 @@
 import { Search, Filter } from 'lucide-react';
 import './DashboardHeader.css';
+import { useState } from 'react'; 
 
-export default function DashboardHeader() {
+export default function DashboardHeader( { outputSearch}) {
+
+const [queary, setQueary] = useState("");
+
+
+//Function for when enter key is pressed.
+function handleKeyPress(e){
+  if(e.key === 'Enter'){
+    outputSearch(queary);
+  }
+}
+
+//Function for button click for search
+function handleSearchClick(){
+  outputSearch(queary);
+}
+
+//Function for clearing search when the input is cleared.
+function handleInputChange(e){
+  setQueary(e.target.value);
+  if(e.target.value.trim() === ""){
+    outputSearch("");
+  }
+}
+
   return (
     <header className="dashboard-header">
       {/* Logo/Title */}
@@ -14,8 +39,15 @@ export default function DashboardHeader() {
             type="text"
             placeholder="Search documents..."
             className="dashboard-header-search-input"
+            value = {queary} //passed string
+            onChange = {handleInputChange} //updates parent element
+            onKeyDown = {handleKeyPress} //handles enter key
           />
-          <Search className="dashboard-header-search-icon" />
+          <Search 
+            className="dashboard-header-search-icon" 
+            onClick={handleSearchClick}
+            style={{ cursor: 'pointer' }}
+            />
         </div>
 
         {/* Filter Button */}
