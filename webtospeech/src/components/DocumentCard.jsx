92fbaeb3f703aafd3264203463e2d667c9b2c855
TypicalJon <jhurley5@my.apsu.edu>
2026-04-09 14:02:38 -0500

    Edit feature

--- webtospeech/src/components/DocumentCard.jsx
+++ webtospeech/src/components/DocumentCard.jsx
@@ -3,12 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import { useEffect, useState , useRef} from 'react';
 import './DocumentCard.css';
 import { createClient } from "@supabase/supabase-js";
+import { createPortal } from 'react-dom';
+
 
 const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_ANON_KEY);
 
 //
 export default function DocumentCard({ id, title, uploadDate, type, fileURL }) {
   const navigate = useNavigate();
+  const [showEdit, setShowEdit] = useState(false);
   
 
   
@@ -27,10 +30,15 @@ export default function DocumentCard({ id, title, uploadDate, type, fileURL }) {
       {/* Menu Button - appears on hover */}
       <ThreeDotMenu
         items= {[
-          { label: 'Edit', onClick: () => console.log('Edit') },
+          { label: 'Edit', onClick: () => setShowEdit(true) },
           { label: 'Delete', onClick: () => {deleteDocument(id, fileURL); console.log('Delete'); } },
         ]}
         /> 
+        {showEdit && (
+          <EditDocument 
+          documentID={id} 
+          onClose={() => setShowEdit(false)} />
+        )}
 
 
       {/* Document info */}
@@ -99,4 +107,62 @@ async function deleteDocument(documentId, fileURL) {
         console.error("Delete Failed:", err);
     }
   }
+
+   function EditDocument({documentID, onClose}) {
+    const [newTitle, setNewTitle] = useState("");
+    const [newAuthor, setNewAuthor] = useState("");
+
+    async function saveChanges() {
+    try {
+
+        //Valid input check
+        if(newTitle.trim() === "" || newAuthor.trim() === ""){
+          alert("Title and Author cannot be empty");
+          return;
+        }
+
+        // This updates the document record in the database TABLE
+        const { error: updateError } = await supabase
+            .from('books')
+            .update({ title: `${newTitle}`, author: `${newAuthor}` })
+            .eq('id', documentID);
+
+        if (updateError) throw updateError;
+
+        console.log("Updated successfully");
+        onClose(); // Close the edit form after saving
+
+    } catch (err) {
+        console.error("Update Failed:", err);
+    }
+  }
+
+    return createPortal(
+      <div className="edit-document-form" onClick={(e) => {e.stopPropagation(); onClose();}}>
+        <div className="edit-document-modal" onClick={(e) => e.stopPropagation()}>
+          <h2>Edit Document</h2>
+          <input type="text" 
+                  placeholder="New Title" 
+                  id="newTitle" 
+                  value={newTitle}
+                  onChange={(e) => setNewTitle(e.target.value)}
+                  />
+          <input type="text" 
+                 placeholder="New Author" 
+                 id="newAuthor" 
+                 value={newAuthor}
+                 onChange={(e) => setNewAuthor(e.target.value)}
+                 />
+              <div className="edit-document-buttons">
+                 <button onClick={onClose}>Cancel</button>
+                 <button onClick={saveChanges}>Save</button>
+              </div>
+        </div>
+      </div>,
+      document.body
+    );
+
+  }
+
+
   
